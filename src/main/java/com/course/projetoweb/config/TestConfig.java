56931985fe2540f83dab510b22fd570e130cbe28
Murilo Barros <barrosmurilo85@gmail.com>
2025-07-22 15:05:42 -0300

    Payment One-To-One association

--- src/main/java/com/course/projetoweb/config/TestConfig.java
+++ src/main/java/com/course/projetoweb/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.course.projetoweb.entities.Category;
 import com.course.projetoweb.entities.Order;
 import com.course.projetoweb.entities.OrderItem;
+import com.course.projetoweb.entities.Payment;
 import com.course.projetoweb.entities.Product;
 import com.course.projetoweb.entities.User;
 import com.course.projetoweb.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
     }
 
 }
