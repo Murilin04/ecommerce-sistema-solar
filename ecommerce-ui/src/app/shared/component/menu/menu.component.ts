413d63674efcaa1f4fd3e643ea20a6b6bf0977fc
Murilo Barros <barrosmurilo85@gmail.com>
2025-08-08 18:58:41 -0300

    wip(header-menu): initial implementation (incomplete)

--- ecommerce-ui/src/app/shared/component/menu/menu.component.ts
+++ ecommerce-ui/src/app/shared/component/menu/menu.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
