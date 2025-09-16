852a86312bfe8b034087492e53465141099db21e
Murilo Barros <barrosmurilo85@gmail.com>
2025-09-16 16:17:40 -0300

    chore(frontend): adjustments on UI layout

--- ecommerce-ui/src/app/auth/login/login.component.ts
+++ ecommerce-ui/src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { FormsModule } from '@angular/forms';
 
