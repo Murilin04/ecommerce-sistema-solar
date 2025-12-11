e635ed75ceacb8196de953eece8ee6aad1e45ed9
Murilo Barros <barrosmurilo85@gmail.com>
2025-12-11 17:19:27 -0300

    refactor(admin): remove component register admin

--- ecommerce-ui/src/app/app.routes.ts
+++ ecommerce-ui/src/app/app.routes.ts
@@ -1,24 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
+
 import { AuthGuard } from './auth/guards/auth-guard/auth.guard';
-import { RegisterComponent } from './auth/register/register.component';
-import { HomeComponent } from './shared/component/home/home.component';
-import { ProfileComponent } from './features/integrador/profile/profile.component';
 import { NoAuthGuard } from './auth/guards/no-auth-guard/no-auth.guard';
-import { EditProfileComponent } from './features/integrador/edit-profile/edit-profile.component';
-import { ResetPasswordFormComponent } from './shared/component/reset-password-form/reset-password-form.component';
-import { SendEmailFormComponent } from './shared/component/send-email-form/send-email-form.component';
 import { RoleGuard } from './auth/guards/role-guard/role.guard';
-import { AdminDashboardComponent } from './auth/admin/admin-dashboard/admin-dashboard.component';
-import { AdminAddNewUsersComponent } from './auth/admin/admin-add-new-users/admin-add-new-users.component';
-import { AdminEditUsersComponent } from './auth/admin/admin-edit-users/admin-edit-users.component';
-import { ProductsComponent } from './features/products/products/products.component';
-import { CartComponent } from './features/products/cart/cart.component';
-import { AdminProductsComponent } from './auth/admin/admin-products/admin-products.component';
-import { CheckoutComponent } from './features/products/chekout/chekout.component';
-import { OrderConfirmationComponent } from './features/products/order-comfirmation/order-comfirmation.component';
-import { MyOrdersComponent } from './features/products/my-orders/my-orders.component';
-import { AdminOrdersComponent } from './auth/admin/admin-orders/admin-orders.component';
 
 export const routes: Routes = [
 
@@ -90,11 +74,6 @@ export const routes: Routes = [
     loadComponent: () => import('./auth/admin/admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent),
     canActivate: [AuthGuard, RoleGuard],
     data: { roles: ['ADMIN'] }
-  },
-  {
-    path: 'admin/register-private&security/true',
-    loadComponent: () => import('./auth/admin/admin-register/admin-register.component').then(m => m.AdminRegisterComponent),
-    canActivate: [NoAuthGuard]
   },
   {
     path: 'admin/produtos',
